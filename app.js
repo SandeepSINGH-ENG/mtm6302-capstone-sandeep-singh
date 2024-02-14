c45183edd261c19d9366db2143c50cd44998d3dc
sandeep <54143606+SandeepSINGH-ENG@users.noreply.github.com>
2024-02-14 18:42:06 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function mobNav(){
+   let nav = document.getElementById("navbar"); 
+   console.log(nav);
+   nav.classList.add("nav-opned");
+} 
+function mobNavclose(){
+    let nav = document.getElementById("navbar"); 
+    console.log(nav);
+    nav.classList.remove("nav-opned");
+ } 
+ 
+
